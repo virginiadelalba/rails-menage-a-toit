3229ed1a937d209705ebdd2eb0d9f542983fd536
Bill Jenkins <bjenkins@anewpt.com>
2020-02-24 17:29:39 +0100

    add the terrace model and validations

---
+++ app/models/terrace.rb
@@ -0,0 +1,9 @@
+class Terrace < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true, uniqueness: true
+  validates :capacity, presence: true
+  validates :price, presence: true
+
+end
---
+++ test/models/terrace_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TerraceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
