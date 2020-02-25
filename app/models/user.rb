b8d2dadcd220f00765c772ae9ce89842264e1bd0
Bill Jenkins <bjenkins@anewpt.com>
2020-02-25 13:21:50 +0100

    corrected user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :terraces
-<<<<<<< HEAD
-=======
   has_many :bookings
->>>>>>> 63a59f815bd22f6f3db5d8ead62b6d966a2db9f5
 end
