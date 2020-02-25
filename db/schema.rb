b7269e5d89fd32dcbe5487adeefe09639b89a7d6
Carolina Cota <carolinareycota@gmail.com>
2020-02-25 10:52:40 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -39,5 +39,6 @@ ActiveRecord::Schema.define(version: 2020_02_24_161218) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
+  add_foreign_key "terraces", "users"
 end
