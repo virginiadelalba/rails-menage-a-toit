fa451ff8ef8949c7858d873f015964f16d7ae74e
Carolina Cota <carolinareycota@gmail.com>
2020-02-27 15:57:57 +0100

    Add JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import { totalPrice } from 'plugins/booking.js'
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 
+//this should be added
 totalPrice();
 initMapbox();
 
