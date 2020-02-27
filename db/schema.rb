0ea4ab03ce69c0c931adc9ef50d563aa3a911dcc
Bill Jenkins <bjenkins@anewpt.com>
2020-02-27 06:17:25 +0100

    geocoder feature added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_112337) do
+ActiveRecord::Schema.define(version: 2020_02_27_050923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_112337) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_terraces_on_user_id"
   end
 
