57b2c4062b618aa4bc96a314c55d1d6b66a58668
Bill Jenkins <bjenkins@anewpt.com>
2020-02-27 14:50:36 +0100

    added map info box and autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,11 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
 initMapbox();
+
+initAutocomplete();
 
 const fp = flatpickr("#start_date", {
   altInput: true,
